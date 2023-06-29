080ca140b0ff9bc72c9edce71177de0081526f77
vahidaa <vahidaa@code.edu.az>
2023-06-29 21:32:45 +0400

    last version of homework3a

---
+++ HomeWork3A/Program.cs
@@ -0,0 +1,26 @@
+﻿int number1, number2; // 1
+
+do
+{
+    Console.Write("1. ədədi girin: "); // 1
+    number1 = Convert.ToInt32(Console.ReadLine()); 
+    Console.Write("2. ədədi girin: "); // 1
+    number2 = Convert.ToInt32(Console.ReadLine());
+
+    if (number1 >= number2)
+    {
+        Console.WriteLine("Yenidən girin. (Birinci ədəd ikincidən kiçik olmalıdır)"); // 1
+        Console.WriteLine(" "); // 1
+    }
+} while (number1 >= number2); // n
+
+int sum = 0; // 1
+
+for (int i = number1; i <= number2; i++)
+{
+    sum += i; // n
+}
+
+Console.WriteLine("Toplam: " + sum); // 1
+
+// 7 + n + n = O(n)
